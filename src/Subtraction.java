9574abb5f64fa0b783b645804eabd7a17c8bccdf
ajaypandhavale <aj40135821@wipro.com>
2025-01-23 18:34:58 +0530

    Subtraction of the two number

---
+++ src/Subtraction.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Subtraction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("enter the number to perform subtraction");
+		int a=sc.nextInt();
+                int b=sc.nextInt();
+		
+		
+		System.out.println("enter the elements to perfrom substraction");
+		
+                int sub =a-b;
+		
+		System.out.println("subtraction  ="+sub);
+		
+			System.out.println("Ajay Dilip Pandhavale");
+
+	}
+
+}
